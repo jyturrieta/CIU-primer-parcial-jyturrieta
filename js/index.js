8a8920abcaf6d0710a02e086c583604c4e38d59b
jyturrieta <joelyturrieta@gmail.com>
2021-09-27 21:09:05 -0300

    variables

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var enviarBoton = document.getElementById("enviar");
 var dniInput = document.getElementById("dni");
+var passwordInput = document.getElementById("password")
 
 enviarBoton.addEventListener('click', function(evento){
-    if(dni.value.length != 8){
+    if(dniInput.value.length != 8){
         alert("El DNI debe tener 8 digitos");
     }
-    else if(password.value.length < 4){
+    else if(passwordInput.value.length < 4){
         alert("La contraseña debe tener 4 o mas caracteres");
         }
     else{
